3477d1aebb461acbb08fd7dea0bff8bb158d466f
ekin <ekinkarakoc1@gmail.com>
2024-02-08 09:40:18 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int r;
+        double pi = 3.14;
+        double alan,cevre;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("yaricap giriniz");
+
+        r = scanner.nextInt();
+
+        alan = pi * r* r;
+        System.out.println("alan: " + alan);
+
+        cevre = 2*pi*r;
+        System.out.println("cevre: " + cevre) ;
+
 
     }
 }
